4ca9392f09c12e31b34705b13cfd560a7fb89428
kanahee <jcapelol@gmail.com>
2021-06-28 18:56:29 -0500

    alt initial files

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import PicksPage from './Components/PicksPage';
 import ResultsPage from './Components/ResultsPage';
 import WeeksDropDown from './Components/WeeksDropDown';
 import LoginForm from './Components/LoginForm'
-import firebase from 'firebase/app';
+import firebase from 'Firebase;
 import "firebase/auth";
 
 import './App.css';
